8088bce56675677c6a49913c21ea209c4dae2ee4
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2023-11-24 14:21:18 -0700

    added weather details and converted to required units

--- script.js
+++ script.js
@@ -15,16 +15,27 @@ function getApi() {
         .then(function (data) {
             console.log(data);
 
-            var searchTerm = searchInput.val()
+            var searchTerm = searchInput.val();
+            var dateUnix = data.dt; // change from unix
+            var date = new Date(dateUnix * 1000).toLocaleDateString()
+            var tempKelvin = data.main.temp; // The API default temperature unit is Kelvin
+            var tempFahrenheit = ((tempKelvin - 273.15) * 1.8 + 32).toFixed(2) // Convert Kelvin temp to Fahrenheit
+            var wind = data.wind.speed; // The API default wind speed unit is meters/sec
+            var windMPH = (wind * 2.2369).toFixed(2) // Convert meters/sec to miles/hour
+            var humidity = data.main.humidity
+            var iconCode = data.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
             console.log(`Searching for ${searchTerm}...`)
             // Create and list search history
             // TODO: Save to local storage
             $('.search-history').append("<ul><ul>").addClass("ul")
             $('.search-history').append('<li>' + city + '</li>').addClass("li")
 
-            $('.main-weather').append(`<h3>${data.name}</h3>`).addClass('cityName');
-            $('.main-weather').append(`<p>${data.weather[0].description}</p>`).addClass('weatherData');
-            $('.main-weather').append(`<p>${data.dt}</p>`).addClass('date');
+            $('.main-weather').append(`<h3>${data.name + ' ' + '(' + date + ')'}</h3>`).addClass('cityName');
+            $('.main-weather').append(`<p> Temp: ${tempFahrenheit}°F</p>`).addClass('weatherData');
+            $('.main-weather').append(`<p> Wind: ${windMPH} MPH</p>`).addClass('weatherData');
+            $('.main-weather').append(`<p> Humidity: ${humidity}%</p>`).addClass('weatherData');
+            
         });
 }
 
@@ -32,6 +43,7 @@ function getApi() {
 // Takes input value of the search bar when user clicks the search button
 // Fetches data connected to the weather API
 searchBtn.click(function (event) {
+    // event.preventDefault();
     getApi();
 })
 
@@ -41,3 +53,4 @@ searchInput.keypress(function (event) {
         searchBtn.click()
     }
 })
+
